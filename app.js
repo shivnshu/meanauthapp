9446a4f792e1905601af4dcabe09e7c250b04f0d
shivnshu <shivanshusinghraj@gmail.com>
2017-02-16 02:35:49 +0530

    routes added

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+const users = require('./routes/users');
+
+const app = express();
+0
+const port = 3000;
+
+// Cors Middleware
+app.use(cors());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+app.get('/', function(req, res){
+    res.send('Invalid Endpoint');
+});
+
+app.listen(port, function() {
+    console.log('Server started on port ' + port);
+});
---
+++ routes/users.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+// Register
+router.get('/register', function(req, res, next){
+    res.send('REGISTER');
+})
+
+// Authenticate
+router.get('/authenticate', function(req, res, next){
+    res.send('AUTHENTICATE');
+})
+
+// Profile
+router.get('/profile', function(req, res, next){
+    res.send('PROFILE');
+})
+
+// Validate
+router.get('/validate', function(req, res, next){
+    res.send('VALIDATE');
+})
+
+
+module.exports = router;
