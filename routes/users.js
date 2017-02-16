e2caaf3d81e59fb43a0d48fbe9abc87bbabcf0f8
shivnshu <shivanshusinghraj@gmail.com>
2017-02-17 00:18:51 +0530

    added models

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
 
 // Register
-router.get('/register', function(req, res, next){
-    res.send('REGISTER');
+router.post('/register', function(req, res, next){
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password3
+    });
+
+    User.addUser(newUser, function(err, user){
+        if(err){
+            res.json({success: false, msg: 'Failed to register user'});
+        }
+        else {
+            res.json({success: true, msg: 'User registered'});
+        }
+    });
 });
 
 // Authenticate
@@ -14,11 +32,6 @@ router.post('/authenticate', function(req, res, next){
 // Profile
 router.get('/profile', function(req, res, next){
     res.send('PROFILE');
-});
-
-// Validate
-router.get('/validate', function(req, res, next){
-    res.send('Vcd ALIDATE');
 });
 
 
