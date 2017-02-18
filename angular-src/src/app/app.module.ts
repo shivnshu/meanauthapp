e2f80f6fcbd26aaf86ea7f311c9e6f5b565af05e
shivnshu <shivanshusinghraj@gmail.com>
2017-02-19 02:13:18 +0530

    added flash messages

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { RegisterComponent } from './components/register/register.component';
 
 // Services Import
 import { ValidateService } from './services/validate.service';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 const appRoutes: Routes = [
   { path:'', component: HomeComponent },
@@ -38,7 +39,8 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule
   ],
   providers: [ValidateService],
   bootstrap: [AppComponent]
--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ValidateService } from '../../services/validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +13,7 @@ export class RegisterComponent implements OnInit {
   email: String;
   password: String;
 
-  constructor(private validateService: ValidateService) { }
+  constructor(private validateService: ValidateService, private flashMessages: FlashMessagesService) { }
 
   ngOnInit() {
   }
@@ -27,13 +28,13 @@ export class RegisterComponent implements OnInit {
 
     // Required fields
     if(!this.validateService.validateRegister(user)){
-      console.log('sdf');
+      this.flashMessages.show('Please fill all fields', {cssClass: 'alert-danger', timeout: 3000});
       return false;
     }
 
     // Validate Email
     if(!this.validateService.validateEmail(user.email)){
-      console.log('sdf EMAIL');
+      this.flashMessages.show('Please use valid email', {cssClass: 'alert-danger', timeout: 3000});
       return false;
     }
 
